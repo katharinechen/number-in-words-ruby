344cb2a209691a4c450d984a784c180056272c52
Katharine Chen <katharinechen.ny@gmail.com>
2014-07-29 21:44:35 -0700

    Refactoring with scales, uncompleted

--- README.md
+++ README.md
@@ -4,3 +4,15 @@
 Create a method that translates numbers in numeric form into written words. For example, it would translate 384 into "three hundred eighty four".
 
 Make it work up to 1 trillion. 
+
+
+I made my first attempt writing this with words, but it seems overly complicated, repeatitive, and vebrose 
+
+when (1000000000000..999999999999999).include?(number)
+		if number%1000000000000 == 0
+			return numbers_in_words(number/1000000000000) + " trillion"
+		else
+			return numbers_in_words(number/1000000000000) + " trillion " + numbers_in_words(number%1000000000000)
+		end 
+
+seemed to verbose 
--- lib/numbers_in_words.rb
+++ lib/numbers_in_words.rb
@@ -6,49 +6,112 @@ def numbers_in_words(number)
 					12 => "twelve", 13 => "thirteen", 14 => "fourteen",
 					15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
 					18 => "eighteen", 19 => "nineteen", 20 => "twenty",
-					30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty",
-					70 => "seventy", 80 => "eighty", 90 => "ninety"
-	}
+					30 => "thirty", 40 => "forty", 50 => "fifty", 
+					60 => "sixty", 70 => "seventy", 80 => "eighty", 
+					90 => "ninety"
+				}
 
-	case
-	when (1000000000000..999999999999999).include?(number)
-		if number%1000000000000 == 0
-			return numbers_in_words(number/1000000000000) + " trillion"
-		else
-			return numbers_in_words(number/1000000000000) + " trillion " + numbers_in_words(number%1000000000000)
-		end
-	when (1000000000..999999999999).include?(number)
-		if number%1000000000 == 0
-			return numbers_in_words(number/1000000000) + " billion"
-		else
-			return numbers_in_words(number/1000000000) + " billion " + numbers_in_words(number%1000000000)
-		end
-	when (1000000..999999999).include?(number)
-		if number%1000000 == 0
-			return numbers_in_words(number/1000000) + "million "
-		else
-			return numbers_in_words(number/1000000) + " million " + numbers_in_words(number%1000000)
-		end
-	when (1000..999999).include?(number)
-		if number%1000 == 0
-			return numbers_in_words(number/1000) + " thousand"
-		else
-			return numbers_in_words(number/1000) + " thousand " + numbers_in_words(number%1000)
-		end
-	when (100..999).include?(number)
-		if number%100 == 0
-			return number_hash[number/100] + " hundred"
-		else
-			return number_hash[number/100] + " hundred " + numbers_in_words(number%100)
-		end
-	when (21..99).include?(number)
-		if number%10 == 0
-			return number_hash[number/10 * 10]
-		else
-			return number_hash[number/10 * 10] + " " + numbers_in_words(number%10)
-		end
-	when (0..20).include?(number)
-		return number_hash[number]
-	end
+	scale_hash = {  1_000_000_000_000 => "trillion", 
+					1_000_000_000 => "billion", 
+					1_000_000 => "million", 
+					1_000 => "thousand", 
+					100 => "hundred" } 	
 
+
+
+	# if number > 100 
+	# 	scale_hash.each do |key, value| 
+	# 		if number/key > 0
+	# 			return "#{number_hash[number/key]} #{value} #{numbers_in_words(number%key)}" if number%key != 0 
+	# 			return "#{number_hash[number/key]} #{value}"
+	# 		end  
+	# 	end  
+	# end
+
+
+	if number > 100 
+		if number/100 > 0 
+			return "#{number_hash[number/100]} hundred #{numbers_in_words(number%100)}" if number%100 != 0 
+			return "#{number_hash[number/100]} hundred"
+		end
+	end 
+
+	if number < 100 && (number_hash.has_key?(number) == false )
+		return "#{number_hash[number/10 * 10]} " + numbers_in_words(number%10) if number%10 != 0 
+		return number_hash[number/10 * 10]	
+	end  
+
+	return number_hash[number] if number_hash.has_key?(number) 
 end
+
+
+puts numbers_in_words(425)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def numbers_in_words(number)
+
+# 	number_hash = { 0 => "zero", 1 => "one", 2 => "two", 3 => "three",
+# 					4 => "four", 5 => "five", 6 => "six", 7 => "seven",
+# 					8 => "eight", 9 => "nine", 10 => "ten", 11 => "eleven",
+# 					12 => "twelve", 13 => "thirteen", 14 => "fourteen",
+# 					15 => "fifteen", 16 => "sixteen", 17 => "seventeen",
+# 					18 => "eighteen", 19 => "nineteen", 20 => "twenty",
+# 					30 => "thirty", 40 => "forty", 50 => "fifty", 60 => "sixty",
+# 					70 => "seventy", 80 => "eighty", 90 => "ninety"
+# 	}
+
+# 	case
+# 	when (1000000000000..999999999999999).include?(number)
+# 		if number%1000000000000 == 0
+# 			return numbers_in_words(number/1000000000000) + " trillion"
+# 		else
+# 			return numbers_in_words(number/1000000000000) + " trillion " + numbers_in_words(number%1000000000000)
+# 		end
+# 	when (1000000000..999999999999).include?(number)
+# 		if number%1000000000 == 0
+# 			return numbers_in_words(number/1000000000) + " billion"
+# 		else
+# 			return numbers_in_words(number/1000000000) + " billion " + numbers_in_words(number%1000000000)
+# 		end
+# 	when (1000000..999999999).include?(number)
+# 		if number%1000000 == 0
+# 			return numbers_in_words(number/1000000) + "million "
+# 		else
+# 			return numbers_in_words(number/1000000) + " million " + numbers_in_words(number%1000000)
+# 		end
+# 	when (1000..999999).include?(number)
+# 		if number%1000 == 0
+# 			return numbers_in_words(number/1000) + " thousand"
+# 		else
+# 			return numbers_in_words(number/1000) + " thousand " + numbers_in_words(number%1000)
+# 		end
+# 	when (100..999).include?(number)
+# 		if number%100 == 0
+# 			return number_hash[number/100] + " hundred"
+# 		else
+# 			return number_hash[number/100] + " hundred " + numbers_in_words(number%100)
+# 		end
+# 	when (21..99).include?(number)
+# 		if number%10 == 0
+# 			return number_hash[number/10 * 10]
+# 		else
+# 			return number_hash[number/10 * 10] + " " + numbers_in_words(number%10)
+# 		end
+# 	when (0..20).include?(number)
+# 		return number_hash[number]
+# 	end
+
+# end
